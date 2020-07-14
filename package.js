e04d369cd474029d782bd3785e345190fefe9988
Jørgen Vatle <Jorgen@WcAServices.net>
2020-07-14 15:55:00 +0300

    Update package name and git URI.

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'lichthagel:accounts-discord',
+  name: 'jorgenvatle:accounts-discord',
   version: '0.2.0',
   // Brief, one-line summary of the package.
   summary: 'Adds account support for Discord',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/Lichthagel/meteor-accounts-discord',
+  git: 'https://github.com/JorgenVatle/meteor-accounts-discord',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
