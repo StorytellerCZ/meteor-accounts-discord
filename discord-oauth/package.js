f4599ee32e4e772617d238aeffa9620b5e860602
Jørgen Vatle <Jorgen@WcAServices.net>
2022-04-07 18:14:37 +0200

    Update dependencies for discord-oauth.

--- discord-oauth/package.js
+++ discord-oauth/package.js
@@ -1,17 +1,17 @@
 Package.describe({
-    name: 'lichthagel:discord-oauth',
+    name: 'jorgenvatle:discord-oauth',
     summary: 'Discord OAuth flow',
-    version: '0.1.0',
+    version: '1.0.0',
     git: 'https://github.com/Lichthagel/meteor-accounts-discord/tree/master/discord-oauth'
 });
 
 Package.onUse(api => {
     api.use('ecmascript@0.12.4', ['client', 'server']);
     api.use('oauth2@1.2.1', ['client', 'server']);
-    api.use('oauth@1.2.7', ['client', 'server']);
+    api.use('oauth@2.1.2', ['client', 'server']);
     api.use('http@1.4.2', 'server');
     api.use('random@1.1.0', 'client');
-    api.use('service-configuration@1.0.11', ['client', 'server']);
+    api.use('service-configuration@1.3.0', ['client', 'server']);
 
     api.addFiles('discord_client.js', 'client');
     api.addFiles('discord_server.js', 'server');
