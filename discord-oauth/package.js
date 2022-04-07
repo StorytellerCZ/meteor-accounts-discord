05fe6385b9d25823f6133d72d3bd3d6c2616178e
Jørgen Vatle <Jorgen@WcAServices.net>
2022-04-07 19:22:07 +0200

    Update http version constraint for discord-oauth

--- discord-oauth/package.js
+++ discord-oauth/package.js
@@ -9,7 +9,7 @@ Package.onUse(api => {
     api.use('ecmascript@0.12.4', ['client', 'server']);
     api.use('oauth2@1.2.1', ['client', 'server']);
     api.use('oauth@2.1.2', ['client', 'server']);
-    api.use('http@1.4.2', 'server');
+    api.use('http@1.4.2 || 2.0.0', 'server');
     api.use('random@1.1.0', 'client');
     api.use('service-configuration@1.3.0', ['client', 'server']);
 
